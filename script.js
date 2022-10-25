83503af041c0b3b201fe6878f807f0eb247df275
username <gjaviles7@gmail.com>
2022-10-25 09:23:07 -0400

    game logic ready, now working on rendering

--- script.js
+++ script.js
@@ -2,23 +2,43 @@
 const disc1 = document.querySelector('#one');
 const disc2 = document.querySelector('#two');
 const disc3 = document.querySelector('#three');
+const tower1 = document.querySelector('#tower-one');
+const tower2 = document.querySelector('#tower-two');
+const tower3 = document.querySelector('#tower-three');
 
 // Arrays to keep track of discs on each tower.
-let towers = {
-    tower1: [disc1, disc2, disc3],
-    tower2: [],
-    tower3: []
-}
+let towers = [
+    {hasDiscs: [disc1, disc2, disc3],
+     towerNumber: 1},
+    {hasDiscs: [],
+     towerNumber: 2},
+    {hasDiscs: [],
+     towerNumber: 3}
+]
 
 // Add event listener to top disc of tower.
-Object.values(towers).forEach(tower => {
-    tower[0].addEventListener('click', moveOptions);
+towers.forEach(tower => {
+    if (tower['hasDiscs'].length !== 0) {
+        tower['hasDiscs'][0].addEventListener('click', moveOptions)
+    }
 });
 
-
-
-
-
+// Manage options available for a player based 
+// on where discs are currently placed.
 function moveOptions() {
-
+    const thisDisc = parseInt(this.getAttribute('data-disc-number'))
+    let availableOptions = [];
+    towers.forEach(tower => {
+        if (tower['hasDiscs'].length !== 0) {
+                if (thisDisc < parseInt(tower['hasDiscs'][0].getAttribute('data-disc-number'))) {
+                    availableOptions.push(tower.towerNumber)
+                }
+        }
+        else if (tower['hasDiscs'].length === 0) {
+            availableOptions.push(tower.towerNumber)
+        }
+    });
+    console.log(availableOptions);
+    // Call modal.
 }
+
