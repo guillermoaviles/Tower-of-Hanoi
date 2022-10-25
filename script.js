3721efef77bf8143c9485d18d339b19ba198d4b6
username <gjaviles7@gmail.com>
2022-10-25 12:16:24 -0400

    imperfect functionality to move discs to different towers

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const disc3 = document.querySelector('#three');
 const tower1 = document.querySelector('#tower-one');
 const tower2 = document.querySelector('#tower-two');
 const tower3 = document.querySelector('#tower-three');
+const modal = document.getElementById("movesAvailable");
+const span = document.getElementsByClassName("close")[0];
+const modalContent = document.querySelector('#modal-content')
 
 // Arrays to keep track of discs on each tower.
 let towers = [
@@ -26,6 +29,8 @@ towers.forEach(tower => {
 // Manage options available for a player based 
 // on where discs are currently placed.
 function moveOptions() {
+    const clickedDisc = this;
+    console.log(clickedDisc);
     const thisDisc = parseInt(this.getAttribute('data-disc-number'))
     let availableOptions = [];
     towers.forEach(tower => {
@@ -39,6 +44,35 @@ function moveOptions() {
         }
     });
     console.log(availableOptions);
-    // Call modal.
+    // Add move options to modal.
+    for (let i = 0; i < availableOptions.length; i++) {
+        if (availableOptions[i] === 1) {
+            const option = document.createElement('button');
+            option.innerHTML = `Tower 1`
+            option.addEventListener('click', () => {
+            tower1.appendChild(clickedDisc);
+            })
+            modalContent.appendChild(option);
+        } else if (availableOptions[i] === 2) {
+            const option = document.createElement('button');
+            option.innerHTML = `Tower 2`
+            option.addEventListener('click', () => {
+            tower2.appendChild(clickedDisc);
+            })
+            modalContent.appendChild(option);
+        } else if (availableOptions[i] === 3) {
+            const option = document.createElement('button');
+            option.innerHTML = `Tower 3`
+            option.addEventListener('click', () => {
+            tower3.appendChild(clickedDisc);
+            })
+            modalContent.appendChild(option);
+        }
+    }
+    // Call modal with available moves.
+    modal.style.display = "block";
+    span.addEventListener('click', () => {
+        modal.style.display = "none";
+    });
 }
 
